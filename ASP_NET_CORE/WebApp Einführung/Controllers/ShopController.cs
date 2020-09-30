80585c767e820fc89e07d50d27996adbfe8d3ee8
Astr0nAT <astron.autnr@gmail.com>
2020-09-30 17:24:30 +0200

    Created basic program to show articles on website

---
+++ ASP_NET_CORE/WebApp Einführung/Controllers/ShopController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp_Einführung.Models;
+
+namespace WebApp_Einführung.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            // normalerweise würden alle Artikel aus einer DB-Tabelle geladen
+            List<Article> actricles = CreateArticleList();
+            return View();
+        }
+
+        private List<Article> CreateArticleList()
+        {
+            return new List<Article>()
+            {
+                new Article(1, "iPhone 11", "Apple", new DateTime(2020, 3, 18), 999.90m, "Handy"),
+                new Article(1, "Aspire", "Acer", new DateTime(2015, 1, 5), 499.90m, "Laptop"),
+                new Article(1, "DeathAdder v2", "Razer", new DateTime(2020, 12, 14), 69.90m, "Elektrogerät"),
+                new Article(1, "ASP.NET MVC", "Apres", new DateTime(2020, 10, 1), 49.90m, "Buch")
+            };
+        }
+    }
+}
